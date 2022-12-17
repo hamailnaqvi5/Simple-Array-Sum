06380f678a6298fa8f2b0c504564e5d4584ce02c
Hamail Naqvi <72408457+hamailnaqvi5@users.noreply.github.com>
2022-12-17 15:34:18 +0500

    SimleArraySum

---
+++ 1- Simple Array Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class ArraySum
+{
+    public static void Main()
+    {
+        int[] a = new int[1000];
+        int i, n, sum = 0;
+        Console.Write("\n\nSum of array:\n");
+        Console.Write("Input total numbers to be stored in the array:");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input {0}  array:\n", n);
+        for (i = 0; i < n; i++)
+        {
+            Console.Write("Number - {0}:", i);
+            a[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        for (i = 0; i < n; i++)
+        {
+            sum += a[i];
+        }
+        Console.Write("Sum of array is : {0}\n\n", sum);
+    }
+}
